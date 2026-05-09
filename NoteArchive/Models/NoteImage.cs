79a7595a09d59732df67392791c85add98dd4fb9
Patricija Videc <160044511+PatricijaVidec@users.noreply.github.com>
2026-05-09 17:26:07 +0200

    Added a view tab for note images

--- NoteArchive/Models/NoteImage.cs
+++ NoteArchive/Models/NoteImage.cs
@@ -9,5 +9,7 @@ public class NoteImage
     public string ImagePath { get; set; } = null!;
 
     public int NoteID { get; set; }
+
+    // warning je zaradi tega kr je tale note lahko null, popravim ko se ostalo naredi
     public Note? Note { get; set; }
 }
